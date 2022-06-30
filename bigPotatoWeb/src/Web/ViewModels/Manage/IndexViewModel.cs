9c79d2a152be232429ab5413d060293f71d76b94
Rafael de Andrade <rdeandrade@microsoft.com>
2022-06-29 23:37:45 -0300

    Imagens
    Ajuste no tratamento de imagens do site.

--- bigPotatoWeb/src/Web/ViewModels/Manage/IndexViewModel.cs
+++ bigPotatoWeb/src/Web/ViewModels/Manage/IndexViewModel.cs
@@ -4,16 +4,18 @@ namespace Microsoft.bigPotatoWeb.Web.ViewModels.Manage;
 
 public class IndexViewModel
 {
+    [Display(Name = "Usuário")]
     public string? Username { get; set; }
 
     public bool IsEmailConfirmed { get; set; }
 
     [Required]
     [EmailAddress]
+    [Display(Name = "E-mail")]
     public string Email { get; set; }
 
     [Phone]
-    [Display(Name = "Número de telefone")]
+    [Display(Name = "Núm. Telefone")]
     public string PhoneNumber { get; set; }
 
     public string? StatusMessage { get; set; }
